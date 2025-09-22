b18477d6baf6194f200d4d1e70e8d72b30599e0f
Manthan Sharma <techmanthansharma@gmail.com>
2025-09-22 19:52:38 +0530

    add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Pizza Shop</h1>
+      <Button variant={"default"}>Hello</Button>
     </div>
   );
 }
