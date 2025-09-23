356cc79cbacc1b74494c7f7c28f2405f949f1789
Manthan Sharma <techmanthansharma@gmail.com>
2025-09-23 13:56:07 +0530

    create product card

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,90 @@
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
+import ProductCard, { Product } from "./components/ProductCard";
 
+const products: Product[] = [
+  {
+    _id: "1",
+    name: "Product 1",
+    description: "Description for Product 1",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 100,
+  },
+  {
+    _id: "2",
+    name: "Product 2",
+    description: "Description for Product 2",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 200,
+  },
+  {
+    _id: "3",
+    name: "Product 3",
+    description: "Description for Product 3",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 150,
+  },
+  {
+    _id: "4",
+    name: "Product 4",
+    description: "Description for Product 4",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 300,
+  },
+  {
+    _id: "5",
+    name: "Product 5",
+    description: "Description for Product 5",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 120,
+  },
+  {
+    _id: "6",
+    name: "Product 6",
+    description: "Description for Product 6",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 180,
+  },
+  {
+    _id: "7",
+    name: "Product 7",
+    description: "Description for Product 7",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 250,
+  },
+  {
+    _id: "8",
+    name: "Product 8",
+    description: "Description for Product 8",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 220,
+  },
+  {
+    _id: "9",
+    name: "Product 9",
+    description: "Description for Product 9",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 170,
+  },
+  {
+    _id: "10",
+    name: "Product 10",
+    description: "Description for Product 10",
+    image:
+      "https://res.cloudinary.com/damktlcfx/image/upload/v1758535912/products/a8b82c54-686b-47f0-b57e-9fd7f6cb9a28.png",
+    price: 280,
+  },
+];
 export default function Home() {
   return (
     <>
@@ -32,7 +115,7 @@ export default function Home() {
       </section>
       <section>
         <div className="container mx-auto py-12">
-          <Tabs defaultValue="pizza" className="w-[400px]">
+          <Tabs defaultValue="pizza">
             <TabsList>
               <TabsTrigger value="pizza" className="text-base">
                 Pizza
@@ -41,8 +124,20 @@ export default function Home() {
                 Beverages
               </TabsTrigger>
             </TabsList>
-            <TabsContent value="pizza">Pizza List</TabsContent>
-            <TabsContent value="beverages">Beverages List</TabsContent>
+            <TabsContent value="pizza">
+              <div className="grid grid-cols-4 gap-6 mt-6">
+                {products.map((product) => {
+                  return <ProductCard product={product} key={product._id} />;
+                })}
+              </div>
+            </TabsContent>
+            <TabsContent value="beverages">
+              <div className="grid grid-cols-4 gap-6 mt-6">
+                {products.map((product) => {
+                  return <ProductCard product={product} key={product._id} />;
+                })}
+              </div>
+            </TabsContent>
           </Tabs>
         </div>
       </section>
